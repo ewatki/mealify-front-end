be4740905c1330089e6791651b970a96e224419e
Lindsay <lindsaybolzwilson@gmail.com>
2023-08-07 16:42:52 -0400

    uncommented navigate

--- components/screens/Login.js
+++ components/screens/Login.js
@@ -1,59 +1,87 @@
 import React, { useContext, createContext } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import axios from 'axios';
 import Register from './Register';
+
 
 const Login = ({ route, navigation }) => {
 
     const [formFields, setFormFields ] = React.useState({
-        username: 'curry',
-        password: 'texturry',
-        pantry: ["ground beef", "cabbage", "green onion", "eggs"],
-        saveRecipes: ["cereal, fishsteak, eggplant curry"],
-        preferences: ["carnivore"]
-
+        email: '',
+        password: '',
     });
+    const [errorMessage, setErrorMessage] = React.useState('')
+    // const UserContext = createContext(formFields);
+    // const user = useContext(UserContext)  
 
-    const UserContext = createContext(formFields);
-    const user = useContext(UserContext)  
-
+    const handleChange = (text, field) => {
+        if (field === 'email') {
+            setFormFields({
+                ...formFields,
+                email: text
+            })
+        } else if (field === 'password') {
+            setFormFields({
+                ...formFields,
+                password: text
+            })
+        }
+    }
     // Verify user input fn - if either inputs aren't a match, show an alert of invalid login
     const handleUserLogin = () => {
-        // axios.post('http://127.0.0.1:8000/user_api/login/')
-        // .then(resp => {
-        // If inputs are valid and if username and password matches database, login user and pass their object down
-        // })
-
-        // Pass down user object
-        navigation.navigate('Main', {user: user})
+        axios.post('https://mealify-zclw.onrender.com/users/login', formFields)
+        .then(response => {
+            console.log('response.data:', response.data)
+            const user = response.data
+            console.log('Successful login!')
+            navigation.navigate('Main', {user: user})
+        })
+        .catch(error => {
+            if (error.response.data === 'Invalid password' || error.response.data === 'That email is invalid') {
+                setErrorMessage(error.response.data)
+                console.log(error.response.data)
+            } else {
+                console.log('error: ', error)
+                setErrorMessage('An error has occured.  Please try again or try another user email and password<br>Also, if at first you dont succeed, try, try again')
+            }
+        })
     }
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.container}>
             <View style={styles.centeredView}>
-                <View>
-                    <Text>Username : </Text>
+                <Text style={styles.header}>Login</Text>
+                <View style={styles.inputContainer}>
                     <TextInput 
-                        value={formFields.username} 
-                        onChangeText={text => setFormFields.username(text)} textContentType="username" autoCompleteType="username" 
-                        style={styles.textbox}
+                        value={formFields.email} 
+                        onChangeText={text => handleChange(text, 'email')} 
+                        // textContentType="email" 
+                        autoCompleteType="email" 
+                        autoCapitalize='none'
+                        placeholder='Email'
+                        style={styles.input}
                     />   
-
-                    <Text>Password :</Text>
                     <TextInput 
                         value={formFields.password} 
-                        onChangeText={text => setFormFields.password(text)} textContentType="password"
-                        style={styles.textbox}
-                    />   
+                        onChangeText={text => handleChange(text, 'password')} 
+                        // textContentType="password"
+                        autoCapitalize='none'
+                        placeholder='Password'
+                        style={styles.input}
+                    />  
                     <Button 
                     title="Login" 
                     onPress={ handleUserLogin } 
                     />
                 </View>
-                <View>
+                <View style={styles.errorContainer}>
+                    <Text style={styles.errorMessage}>{errorMessage}</Text>
+                </View>
+                <View style={styles.registerContainer}>
                     <Text>Don't have an account? </Text>
                     <TouchableOpacity onPress={ () => navigation.navigate(Register) }> 
-                        <Text>Register</Text>
+                        <Text style={styles.registerButton}>Register</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -66,5 +94,54 @@ export default Login;
 const styles = StyleSheet.create({
     container: {
         marginTop: 50,
+        flex: 1, 
+        // alignItems: 'center', 
+        // justifyContent: 'center'
+    },
+    centeredView: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    errorContainer: {
+        flex: 1, 
+        alignItems: 'center',
+        padding: 20,
+    },
+    errorMessage: {
+        color: 'red',
+        justifyContent: 'center'
+    },
+    header:{
+        flex: 1, 
+        justifyContent: 'flex-start',
+        fontSize: 40,
+        fontWeight: 'bold'
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        // width: '50%',
+        // justifyContent: 'center',
+        // alignItems: 'center'
+    },
+    inputContainer: {
+        flex:1,
+    },
+    registerButton: {
+        borderWidth: 1,
+        marginTop: 3,
+        padding: 5,
+        paddingLeft: 7,
+        borderRadius: 10,
+    },
+    registerContainer: {
+        flex: 1,
+        paddingBottom: 50,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     }
-    });
+});
